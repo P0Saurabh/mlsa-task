e499d71dd6e1088a286759cece37a295007ce260
P0Saurabh <111894219+P0Saurabh@users.noreply.github.com>
2024-05-08 11:13:20 +0000

    done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/computer-3174729_1280.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "la[tp[]] background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a AI/ML Student.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,9 +32,9 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
+  "Ai/ml",
   "Inclusive design",
-  "Focus group testing",
+  "",
   "Mobile user interfaces",
   "Graphic design",
 ];
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in AI/ML.";
 
 const About = () => {
   return (
