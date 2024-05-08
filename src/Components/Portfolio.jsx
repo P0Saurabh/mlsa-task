891c2e914553f55fd3a5290a73c37ab6b0d9e566
P0Saurabh <111894219+P0Saurabh@users.noreply.github.com>
2024-05-08 11:21:21 +0000

    n

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/ai-7265469_1280.jpeg";
+import image from "../images/ai-7265469_1280.jpg";
 
 const imageAltText = "ai image";
 
